2d7774ae50ada21aa674696125b19d46a908af5b
xursandbek <a18291539@gmail.com>
2025-11-01 14:17:04 +0500

    Responsive

--- app5.js
+++ app5.js
@@ -1,9 +1,9 @@
 document.getElementById("form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    document.getElementById("successMsg").style.display = "block";
-    this.reset();
-    setTimeout(() => {
-      document.getElementById("successMsg").style.display = "none";
-    }, 3000);
-  });
-  
+  e.preventDefault();
+  const successMsg = document.getElementById("successMsg");
+  successMsg.style.display = "block";
+  this.reset();
+  setTimeout(() => {
+    successMsg.style.display = "none";
+  }, 3000);
+});
